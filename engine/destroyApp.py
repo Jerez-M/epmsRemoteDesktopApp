746f7fd8b426eea8218120a79bda6d5b4145717c
Jeremiah Muchazondida <jjmuchazondida@gmail.com>
2023-09-01 19:15:22 +0200

    All committed

---
+++ engine/destroyApp.py
@@ -0,0 +1,26 @@
+import sys
+import json
+import moni
+import pymysql
+# import mysql.connector
+from datetime import date
+
+connectiondb = pymysql.connect(host="localhost", user="Jeremiah", password="0202February.", database="employees", port=3306)
+# connectiondb = mysql.connector.connect(host="localhost", user="Jeremiah",
+#                                        password="0202February.", database="employees", port=3306)
+cursordb = connectiondb.cursor()
+print(connectiondb)
+
+json_object = json.loads(sys.argv[1])
+
+e_id = json_object["e_id"]
+o_id = json_object["o_id"]
+
+for w, t in moni.show_activity():
+    today = date.today()
+    sql = "INSERT INTO MonitoringDetails (md_title, md_total_time_seconds, md_date, e_id_id, o_id_id) VALUES (%s, %s, %s, %s, %s)" 
+    val = (w,t, today, e_id, o_id)
+    cursordb.execute(sql, val)
+    connectiondb.commit()
+    print(cursordb.rowcount, "record inserted.")
+
